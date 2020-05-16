3ba343d73e26319d9834aba2c87d5b30e26b91f0
jared1291 <58518410+jared1291@users.noreply.github.com>
2020-05-16 18:52:11 -0400

    Create PandemicCreature.java

---
+++ PandemicCreature.java
@@ -0,0 +1,37 @@
+
+import java.awt.Color;
+
+public class PandemicCreature extends PandemicLifeForm {
+
+	private int currentSizeOfCreatureList = myWorld.getCreatureList().size() + 1;
+
+	public PandemicCreature(PandemicLocation myLocation, PandemicWorld myWorld) {
+		super(myLocation, myWorld);
+		myColor = Color.orange;
+	}
+
+
+	public void move() {
+
+		for (int creature = 0; creature < currentSizeOfCreatureList; creature++) {
+			if (myWorld.getCreatureList().get(creature).getMyColor() == Color.orange) {
+
+				int currX = myWorld.getCreatureList().get(creature).getMyLocation().getX();
+				int currY = myWorld.getCreatureList().get(creature).getMyLocation().getY();
+
+				// Allows movement of up to 1 step forward or backward
+				int stepsX = (int) (Math.random() * 2) - 1;
+				int stepsY = (int) (Math.random() * 2) - 1;
+
+				currX += stepsX;
+				currY += stepsY;
+
+				if (0 <= currX && currX <= 100 && 0 <= currY && currY <= 100) {
+					myWorld.getCreatureList().get(creature).myLocation.setX(currX);
+					myWorld.getCreatureList().get(creature).myLocation.setY(currY);
+				}
+			}
+		}
+	}
+
+}
